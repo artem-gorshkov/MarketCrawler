e11d1258b0aa10dfe350f0cc99e26fe1226307a2
Даниил Пушкарев <d.pushkarev@decosystems.ru>
2023-05-29 01:30:42 +0300

    Add tradeit parser

---
+++ parsers/tradeit.py
@@ -0,0 +1,55 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import requests
+
+
+class TradeIt:
+    URL = 'https://tradeit.gg/api/v2/inventory/data?gameId=730&sortType=Popularity&searchValue' \
+          '=&minPrice=0&maxPrice=100000&minFloat=0&maxFloat=1&showTradeLock=true&colors=&showUserListing=true&fresh' \
+          '=false&'
+
+    HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+
+    def __init__(self):
+        self._session = requests.Session()
+
+    def _get_page(self, left: int, step: int):
+        param = f'offset={left}&limit={step}'
+        response = self._session.get(self.URL + param, headers=self.HEADER)
+        items = response.json()['items']
+
+        parsed_items = list(map(self._parse_item, items))
+
+        return parsed_items
+
+    def _parse_item(self, item: dict):
+        return {
+            'name': item['name'],
+            'price': item['price'],
+            'stattrack': item.get('hasStattrak'),
+            'market_cup': item.get('currentStock')
+        }
+
+    @staticmethod
+    def _create_intervals(n: int) -> tuple[list, int]:
+        MAX_ITEMS = 4000
+        step = MAX_ITEMS // n
+
+        return [i + step * i for i in range(n)], step
+
+    def update_market_status(self, n_workers=3):
+        result = []
+        with ThreadPoolExecutor(max_workers=n_workers) as executor:
+            futures = []
+            intervals, step = self._create_intervals(n_workers)
+            for left in intervals:
+                futures.append(executor.submit(self._get_page, left, step))
+            for futures in as_completed(futures):
+                result.extend(futures.result())
+
+        print(result)
+
+
+if __name__ == '__main__':
+    instance = TradeIt()
+    instance.update_market_status(5)
