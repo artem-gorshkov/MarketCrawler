ec7198a15215dbfa50f5c4d7948986d443442502
Daniil <pushkarevd.89@gmail.com>
2023-06-12 16:10:03 +0000

    интеграция с s3

--- src/pyspark/spark.py
+++ src/pyspark/spark.py
@@ -2,6 +2,9 @@ import os
 
 from pyspark.sql import SparkSession
 import pandas as pd
+from datetime import datetime, timedelta
+
+TABLE_NAMES = ['csgomarket', 'lisskins', 'pairs', 'skinbaron', 'tradeit']
 
 
 class SparkCustomeSession:
@@ -28,27 +31,63 @@ class SparkCustomeSession:
     def spark(self) -> SparkSession:
         return self._spark
 
-    def jdbc_read(self, table_name: str):
+    def jdbc_read(self, query: str):
         df = self._spark.read \
             .format("jdbc") \
-            .option("url", "jdbc:postgresql://localhost:5432/market") \
-            .option("dbtable", table_name) \
+            .option("url", "jdbc:postgresql://192.168.0.29:5432/market") \
+            .option("dbtable", query) \
             .option("user", "postgres") \
             .option("password", "changeme") \
             .option("driver", "org.postgresql.Driver") \
             .load()
 
         df.printSchema()
+        return df
 
-    def s3_write(self):
-        df = self._spark.createDataFrame(pd.DataFrame({'a': [1, 2, 3], 'b': [4, 2, 1]}))
-        df.show()
-        # df = self._spark.read.csv("s3a://market/wireshark.csv")
-        # df.show()
-        df.write.format('csv').options(delimiter='|').mode('overwrite').save('s3a://market/test4.csv')
+    def s3_write(self, df, path):
+        df.write.mode('overwrite').parquet('s3a://market/' + path)
+
+    def s3_read(self, path):
+        return self._spark.read.parquet('s3a://market/' + path)
+
+    def create_path(self, table):
+        return datetime.today().strftime('%Y/%m/%d/') + table
+
+    def create_migrartion_query(self, date, table):
+        return f'''
+        (with data_per_day as ( 
+             select 
+              * 
+             from market.{table} 
+             where price_timestamp between to_date('{date}', 'YYYYMMDD')::timestamp and to_date('{date}', 'YYYYMMDD') + interval '1 day - 1 millisecond'  
+            ) 
+            select * from data_per_day) as df
+        '''
+
+    def legacy_migration(self):
+        for table in TABLE_NAMES:
+            for date in range(20230607, 20230612):
+                query = self.create_migrartion_query(date, table)
+                df = self.jdbc_read(query)
+                path = datetime.strptime(str(date), '%Y%m%d').strftime('%Y/%m/%d/') + table
+                print(path)
+                self.s3_write(df, path)
+
+    def do_migration(self):
+        for table in TABLE_NAMES:
+            now = datetime.now() - timedelta(days=1)
+            query = self.create_migrartion_query(now.strftime('%Y%m%d'), table)
+            df = self.jdbc_read(query)
+            path = self.create_path(table)
+            print(path)
+            self.s3_write(df, path)
+
+    def migrate_item_etln(self):
+        df = self.jdbc_read('(select * from market.item_etln) as df')
+        self.s3_write(df, 'item_etln')
 
 
 if __name__ == "__main__":
     spark = SparkCustomeSession()
-    # spark._spark.sparkContext.setLogLevel('INFO')
-    spark.s3_write()
+    # spark._spark.sparkContext.setLogLevel('DEBUG')
+    spark.do_migration()
